ba945ed602913262308b31bcccfd4160f0d49e79
cargnieli2016 <98849490+cargnieli2016@users.noreply.github.com>
2023-08-12 06:58:22 -0300

    Add files via upload

---
+++ ForLista.java
@@ -0,0 +1,16 @@
+class ForLista {
+    public static void main(String[] args) {
+
+        String [] frutas = {
+                "Maça",
+                "Banana",
+                "Tomate",
+                "Mamão"
+        };
+
+        for (int i = 0; i < frutas.length ; i++) {
+            System.out.println(frutas[i]);
+
+        }
+    }
+}
