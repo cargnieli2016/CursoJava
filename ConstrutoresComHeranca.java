818760ff9840b3520b7a21d5e1cb01796bb75e24
cargnieli2016 <98849490+cargnieli2016@users.noreply.github.com>
2023-08-21 08:54:23 -0300

    Add files via upload
    Construtor com herança.

---
+++ ConstrutoresComHeranca.java
@@ -0,0 +1,69 @@
+//ConstrutoresComHeranca
+class Animal { // Classe pai -> SUPER classe
+    String cor;
+    int tamanho;
+    int quantidadePatas;
+    double peso;
+    public Animal() { // Construtor vazio
+    }
+    public Animal(String cor, int tamanho, int qtdePatas, double peso) { // Construtor com parametros
+        this.cor = cor;
+        this.tamanho = tamanho;
+        this.quantidadePatas = qtdePatas;
+        this.peso = peso;
+    }
+    void correr() {
+        System.out.println("Correndo como um ");
+    }
+    void dormir() {
+        System.out.println("Dormindo");
+    }
+}
+class Cachorro extends Animal { // Classe filha ou subclasse
+
+    // Atributos específicos para o cachorro
+
+    // Construtor
+    public Cachorro() { // Construtor vazio
+
+    }
+    public Cachorro(String cor, int tamanho, int qtdePatas, double peso) { //
+        super(cor, tamanho, qtdePatas, peso);
+    }
+    void latir() {
+        System.out.println("Latir");
+    }
+    //Sobrecarga de método
+    void latir(String pessoa) {
+        System.out.println("Latir para " + pessoa);
+    }
+    // Sobrescrita de método -> Sobreposição
+    @Override
+    // Sobrescrito da classe pai
+    void correr() {
+        super.correr(); // Implementação padrão
+        System.out.println("Cachorro com " + this.quantidadePatas + " patas.");
+    }
+}
+class Passaro extends Animal { // Classe filha ou subclasse
+    public Passaro() { // Construtor vazio
+    }
+    public Passaro(String cor, int tamanho, int qtdePatas, double peso) { // Construtor com parametros
+        super(cor, tamanho, qtdePatas, peso);
+    }
+    void voar() {
+        System.out.println("Voando");
+    }
+    // Sobrescrito da classe pai
+    @Override
+    void correr() {
+        super.correr();
+        System.out.println("Pássaro utilizando as asas e com " + this.quantidadePatas + " patas.");
+    }
+}
+class ConstrutoresComHeranca {
+    public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro("Preto", 8, 4, 55);
+        cachorro.correr();
+    }
+}
