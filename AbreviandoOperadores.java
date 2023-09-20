dcf201702902bf448a1dc152bd689791bdc4f260
cargnieli2016 <98849490+cargnieli2016@users.noreply.github.com>
2023-09-20 06:25:53 -0300

    Abreviando operadores
    Abreviando operadores

---
+++ AbreviandoOperadores.java
@@ -0,0 +1,14 @@
+
+public class AbreviandoOperadores {
+    public static void main(String[] args) {
+        int numero = 20;
+        numero += 1;
+        numero *= 2;
+        numero /= 2;
+        numero -=1;
+        numero %=2;
+
+        System.out.println(numero);
+
+    }
+}
