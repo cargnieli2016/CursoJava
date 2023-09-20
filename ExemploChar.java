d9c0929110d248be437923ab7b00419c03951556
cargnieli2016 <98849490+cargnieli2016@users.noreply.github.com>
2023-09-20 07:10:43 -0300

    Exemplo char

---
+++ ExemploChar.java
@@ -0,0 +1,11 @@
+public class ExemploChar {
+    public static void main(String[] args) {
+        char inicialDoNome = 'W'; // aspas simples
+        char tipoCliente = '2';
+        char simbolo = '@';
+
+        System.out.println("Inicial do nome: " + inicialDoNome);
+        System.out.println("Tipo cliente: " + tipoCliente);
+        System.out.println("Símbolo: " + simbolo);
+    }
+}
