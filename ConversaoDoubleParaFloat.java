060e84cd56317c08a91b9c770fb7e9655dc9dde2
cargnieli2016 <98849490+cargnieli2016@users.noreply.github.com>
2023-09-21 07:56:35 -0300

    Conversao double para float

---
+++ ConversaoDoubleParaFloat.java
@@ -0,0 +1,12 @@
+public class ConversaoDoubleParaFloat {
+    public static void main(String[] args) {
+        double peso1 = 20.5;
+        float peso2 = (float) peso1;
+
+        float taxa1 = 934.5F;
+        double taxa2 = taxa1;
+        
+        System.out.println(peso1);
+        System.out.println(taxa2);
+    }
+}
